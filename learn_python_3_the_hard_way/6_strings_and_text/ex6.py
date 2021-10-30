bf3e442ea1439ca97b57e4ff99821934bea13f0d
Bence Bognar <beni20180722@gmail.com>
2021-10-30 16:32:46 +0200

    6

--- learn_python_3_the_hard_way/6_strings_and_text/ex6.py
+++ learn_python_3_the_hard_way/6_strings_and_text/ex6.py
@@ -15,9 +15,9 @@ print(f"I also said: '{y}'")
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! {}"
 
-print(joke_evaluation.format(hilarious))
+print(joke_evaluation.format(hilarious)) # hilarious variable goes to the first {}
 
 w = "This is the left side of..."
 e = "a string with a right side."
 
-print(w + e)
+print(w + e) # string concat
