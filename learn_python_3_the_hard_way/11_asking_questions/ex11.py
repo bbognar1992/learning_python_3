cc8d61bcaffa4ff9811d0f735fb5e5f431404316
Bence Bognar <beni20180722@gmail.com>
2021-11-06 14:48:37 +0100

    11

---
+++ learn_python_3_the_hard_way/11_asking_questions/ex11.py
@@ -0,0 +1,8 @@
+print("How old are you? (years)", end=' ')
+age = input()
+print("How tall are you? (cm)", end=' ')
+height = input()
+print("How much do you weigh? (kg)", end=' ')
+weight = input()
+
+print(f"So, you're {age} years old, {height} cm tall and {weight} kg heavy.")
