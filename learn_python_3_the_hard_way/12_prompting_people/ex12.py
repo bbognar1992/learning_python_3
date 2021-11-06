8c364ca1c7cb43266b51c10979605d56075bb488
Bence Bognar <beni20180722@gmail.com>
2021-11-06 15:00:10 +0100

    12

---
+++ learn_python_3_the_hard_way/12_prompting_people/ex12.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, your name is {name}, And you are {age} old, {height} tall and {weight} heavy.")
