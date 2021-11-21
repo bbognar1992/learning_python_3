e0d98bc18bf38796914cdf63e5a4ff359f3c4917
Bence Bognar <beni20180722@gmail.com>
2021-11-21 09:45:00 +0100

    18 changed

--- learn_python_3_the_hard_way/18_names,variables,code,functions/ex18.py
+++ learn_python_3_the_hard_way/18_names,variables,code,functions/ex18.py
@@ -3,8 +3,8 @@ def print_two(*args):
     arg1, arg2 = args
     print(f"arg1: {arg1}, arg2: {arg2}")
 
-    # ok, that *args is actually pointless, we can just do this
- def print_two_again(arg1, arg2):
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
      print(f"arg1: {arg1}, arg2: {arg2}")
 
 # this just takes one argument
@@ -17,5 +17,7 @@ def print_none():
 
 print_two("Zed","Shaw")
 print_two_again("Zed","Shaw")
+a=('One','Two')
+print_two_again(*a)
 print_one("First!")
 print_none()
