e55b46ba80778ef005fcabf61ec24336b80b9a3c
Bence Bognar <beni20180722@gmail.com>
2021-12-08 15:44:57 +0100

    34

---
+++ learn_python_3_the_hard_way/34_accesing_elements_of_lists/ex34.py
@@ -0,0 +1,12 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# cardinal vs ordinal numbering
+
+print(animals[1])
+print(animals[2])
+print(animals[0])
+print(animals[3])
+print(animals[4])
+print(animals[2])
+print(animals[5])
+print(animals[4])
